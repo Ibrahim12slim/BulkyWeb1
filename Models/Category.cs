cf92aae331c0658576a55ca7c3724cfa56e146b0
Ibrahim12slim <96397533+Ibrahim12slim@users.noreply.github.com>
2023-12-15 09:44:32 +0200

    Adding: Microsoft.EntityFrameworkCore Microsoft...Tools Microsoft...SqlServer

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+    [Key] //ID will be the primary key of the table Category , Rider is smart when the attr contains id then without KEY will take it as primary.
+    public int Id { get; set; }
+    [Required] //name : not null its required.
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
