dad56318a76c09b37775c96e13d13f11c10ca3ad
Ibrahim12slim <96397533+Ibrahim12slim@users.noreply.github.com>
2023-12-16 09:02:10 +0200

    Adding category choice in the top Adding category records to the database adding migration called SeedCategoryTable and then updating the database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data;
+//Class ApplicationDbContext : connection between the Entity Framework and the database.
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) :base (options)
+    {
+        
+    }
+    
+    //create table categories 
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Telling EF we need to add these 3 records of categorie
+        //add migration now then update database
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+        );
+    }
+}
